f260b952435904482f274a2833fd15244b216b82
Richard Outten <outtenr@gmail.com>
2013-10-27 22:19:13 -0400

    Initial start with grunt, bower and libs.
    Base directory for using grunt and bower.  Pulls in modernizr and
    backbone.

---
+++ Gruntfile.js
@@ -0,0 +1,158 @@
+'use strict';
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function (grunt) {
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    watch: {
+      options: {
+        nospawn: true
+      },
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: [
+          'app/*.html',
+          '{.tmp,app}/styles/{,*/}*.css',
+          '{.tmp,app}/scripts/{,*/}*.js',
+          'app/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+          'app/scripts/templates/*.{ejs,mustache,hbs}'
+        ]
+      },
+    },
+    connect: {
+      options: {
+        port: 9000,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, "app")
+            ];
+          }
+        }
+      },
+      test: {
+        options: {
+          port: 9001,
+          middleware: function (connect) {
+            return [
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'test'),
+              mountFolder(connect, "app")
+            ];
+          }
+        }
+      },
+      dist: {
+        options: {
+          middleware: function (connect) {
+            return [
+              mountFolder(connect, "dist")
+            ];
+          }
+        }
+      }
+    },
+    clean: {
+      dist: ['.tmp', 'dist/*'],
+      server: '.tmp'
+    },
+    useminPrepare: {
+      html: 'app/index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+    usemin: {
+      html: ['dist/{,*/}*.html'],
+      css: ['dist/styles/{,*/}*.css'],
+      options: {
+        dirs: ['dist']
+      }
+    },
+    cssmin: {
+      dist: {
+        files: {
+          'dist/styles/main.css': [
+            '.tmp/styles/{,*/}*.css',
+            'app/styles/{,*/}*.css'
+          ]
+        }
+      }
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'app',
+          dest: 'dist',
+          src: [
+            '*.{ico,txt}',
+            '.htaccess',
+            'images/{,*/}*.{webp,gif}',
+            'styles/fonts/{,*/}*.*'
+          ]
+        }]
+      }
+    },
+    bower: {
+      all: {
+        rjsConfig: 'app/scripts/main.js'
+      }
+    },
+    rev: {
+      dist: {
+        files: {
+          src: [
+            'dist/scripts/{,*/}*.js',
+            'dist/styles/{,*/}*.css',
+            'dist/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+            '/styles/fonts/{,*/}*.*'
+          ]
+        }
+      }
+    }
+    
+  });
+
+
+  grunt.registerTask('server', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+    } else if (target === 'test') {
+      return grunt.task.run([
+        'clean:server',
+        'connect:test:keepalive'
+      ]);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+
+  grunt.registerTask('default', [
+    'test',
+    'build'
+  ]);
+
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+## Getting Started
+
+Install browser js files:
+
+    > bower install
+
+Start a local server:
+
+    > grunt server
+
+Visit the site in your browser:
+
+    http://127.0.0.1:9000/
+
+## Development
+
+Try editing app/index.html and you should see the livereload take
+place.  This seemed to work in FF for me, but you might need Chrome
+with the livereload plugin.
